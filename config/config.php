48789c6a6173abf5ad8bb975f734e5afa08d7aae
Damien PIQUET <damien.piquet@forlabs.fr>
2022-02-24 13:49:37 +0100

    array config support

--- config/config.php
+++ config/config.php
@@ -7,6 +7,24 @@
  *
  */
 
+
+function environmentVariablesParseEnvValue($settingEnvName, $isArray = false)
+{
+    $envValue = getenv($settingEnvName);
+    if ($envValue === false) { return false; }
+
+    // String
+    if ($isArray === false) { return $envValue; }
+
+    // Array
+    $arrayValue = array_map(function($item) {
+        return rtrim($item);
+    }, explode("\n", $envValue));
+
+    // Filter empty strings that creep into env line separated arrays
+    return array_filter($arrayValue, function ($item) { return $item !== ""; });
+}
+
 return array(
     'Piwik\Config' => DI\decorate(function ($previous, \Psr\Container\ContainerInterface $c) {
         $settings = $c->get(\Piwik\Application\Kernel\GlobalSettingsProvider::class);
@@ -15,12 +33,24 @@ return array(
         $all = $ini->getAll();
         foreach ($all as $category => $settings) {
             $categoryEnvName = 'MATOMO_' . strtoupper($category);
-            foreach ($settings as $settingName => $value) {
-                $settingEnvName  = $categoryEnvName . '_' .strtoupper($settingName);
 
-                $envValue = getenv($settingEnvName);
+            foreach ($settings as $settingName => $value) {
+                $general = $previous->$category;
+
+                // String
+                $settingEnvName  = $categoryEnvName . '_' .strtoupper($settingName);
+                $envValue = environmentVariablesParseEnvValue($settingEnvName);
                 if ($envValue !== false) {
-                    $general = $previous->$category;
+                    $general[$settingName] = $envValue;
+                    $previous->$category = $general;
+                    // Do not try to load as an array if we already found it as string
+                    continue;
+                }
+
+                // Array
+                $settingEnvName  = $categoryEnvName . '_' . strtoupper($settingName) . '_ARRAY';
+                $envValue = environmentVariablesParseEnvValue($settingEnvName, true);
+                if ($envValue !== false) {
                     $general[$settingName] = $envValue;
                     $previous->$category = $general;
                 }
